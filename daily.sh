d3104e67e42740922b96702c4d9caab475661e65
Santiago Bonada <Santiago.Bonada@uoit.net>
2016-04-01 14:54:35 -0400

    initialized backend folder, place the backend of our choosing there. Initialized daily.sh, our daily script which runs our program

---
+++ daily.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+#TODO: run frontend over a number of banking transactions
+#save to seperate files
+#at end concatenate output files
+
+#TODO: run backend over concatenated file
+
+echo "Implement the script to run the daily system here"
