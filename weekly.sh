87ca8c8198ca86e7ab9cdaed8d0b89c158c55c5c
judacribz <sheron.balas@gmail.com>
2016-04-08 14:47:00 -0400

    chages to weekly

--- weekly.sh
+++ weekly.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-./daily.sh
+#path setup.
+path=$(pwd)
+front=$path/frontend
+back=$path/backend
+txn=$path/txns
+in=$path/inputs
+acc=$path/masterBankAccounts
+[ -d $acc ] || mkdir $acc
+
+
+#makes Master bank accounts directory for each day.
+for i in {0..4}
+do
+    #./daily.sh $in/input${i}
+    ./daily.sh
+    cp $back/MasterBankAccounts.dat $acc/MasterBankAccounts.day${i}
+done
