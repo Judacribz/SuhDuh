f1b16c34be1144bf69e212af7e5e17926f1825fa
judacribz <sheron.balas@gmail.com>
2016-04-11 17:35:37 -0400

    finished! check deliverables

---
+++ deliverables/5_Daily_Runs/weekly.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#path setup.
+path=$(pwd)
+front=$path/frontend
+back=$path/backend
+txn=$path/txns
+in=$path/inputs
+acc=$path/masterBankAccounts
+[ -d $acc ] || mkdir $acc
+
+
+#makes Master bank accounts directory for each day.
+for i in {0..4}
+do
+    ./daily.sh $in/inputs${i}
+
+    cp $back/MasterBankAccounts.dat $acc/MasterBankAccounts.day${i}
+done
